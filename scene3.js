94d52bf651cc4531cb5de8a9061cc5b0d0eb443c
usha1639 <usha1639@gmail.com>
2022-07-31 15:44:35 -0400

    Update scene3.js

--- scene3.js
+++ scene3.js
@@ -18,7 +18,7 @@ function loadScene3(state) {
         .attr("class", "tooltip")
         .style("opacity", 0);
 
-    d3.csv("usCounty2020.csv", function(data) {
+    d3.csv("https://github.com/usha1639/NarrativeVisualizaton/blob/main/usCounty2020.csv", function(data) {
         console.log("read csv for scene3");
         data.forEach(d => {
             d.cases = +d.cases;
